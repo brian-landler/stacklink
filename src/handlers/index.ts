bd158d890dbfc3215311d4819fa3eb4d753432b1
Brian Landler <blandler@fi.uba.ar>
2025-04-27 23:25:21 +0200

    added handle for User interface, password hashing, and duplication checking rule for handle

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,18 +1,37 @@
 import type { Request, Response } from 'express'
+import slug from 'slug'
 import User from "../models/User"
+import { hashPassword } from '../utils/auth'
+import { validationResult } from 'express-validator'
 
 export const createAccount = async (req: Request, res: Response) => {
-    const { email } = req.body
+    
+    let errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.status(400).json({errors: errors.array()})
+        return
+    }
+    
+    const { email, password } = req.body
 
     const userExists = await User.findOne({email})
     if (userExists) {
-        const error = new Error('User already exists')
-        return res.status(409).json({error: error.message})
+        const error = new Error('The email account is already in use')
+        res.status(409).json({error: error.message})
+        return
     }
 
-    let user = new User(req.body)
-    
-    await user.save()
+    const handle = slug(req.body.handle, '')
+    const handleExists = await User.findOne({handle})
+    if (handleExists) {
+        const error = new Error('Unavailable user name')
+        res.status(409).json({error: error.message})
+        return
+    }
 
+    const user = new User(req.body)
+    user.password = await hashPassword(password)
+    user.handle = handle
+    await user.save()
     res.status(201).send('User created successfully') 
 }
