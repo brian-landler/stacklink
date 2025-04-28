690e65f3a0b9709bdc49ad24353c6ec8538222e5
Brian Landler <blandler@fi.uba.ar>
2025-04-28 22:56:13 +0200

    added validation for handle, name, email and password for user registration

--- src/router.ts
+++ src/router.ts
@@ -5,8 +5,19 @@ import { createAccount } from './handlers'
 const router = Router()
 
 /** Auth **/
-router.post('/auth/register', 
-    body('handle').notEmpty(),
+router.post('/auth/register',
+    body('handle')
+        .notEmpty()
+        .withMessage('Handle can not be empty'),
+    body('name')
+        .notEmpty()
+        .withMessage('Name can not be empty'),
+    body('email')
+        .isEmail()
+        .withMessage('Invalid email address'),
+    body('password')
+        .isLength({min:8})
+        .withMessage('Name can not be empty'),
     createAccount)
 
 export default router
