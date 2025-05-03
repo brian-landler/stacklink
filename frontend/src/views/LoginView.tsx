6b81ae948325c16ed8cc768c93c79be96d7d3ba9
Brian Landler <blandler@fi.uba.ar>
2025-05-03 19:31:05 +0200

    Set up basic structure for login and register views

---
+++ frontend/src/views/LoginView.tsx
@@ -0,0 +1,5 @@
+export default function LoginView() {
+    return (
+        <div className="text-9xl">Login view</div>
+    )
+}
---
+++ frontend/src/views/RegisterView.tsx
@@ -0,0 +1,5 @@
+export default function RegisterView() {
+    return (
+        <div className="text-9xl">Register view</div>
+    )
+}
