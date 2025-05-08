3ad7c6eddd5cc027e90b1fc30bf2cbf72677ff35
Brian Landler <blandler@fi.uba.ar>
2025-05-08 23:02:13 +0200

    Added ProfileForm type, added description field to User model, added form validation for ProfileView

--- frontend/src/views/ProfileView.tsx
+++ frontend/src/views/ProfileView.tsx
@@ -1,12 +1,27 @@
+import { useForm } from "react-hook-form";
+import { useQueryClient } from "@tanstack/react-query";
 import { PrimaryButtonForm } from "@/components/buttons";
-
+import ErrorMessage from "@/components/ErrorMessage";
+import { ProfileForm, User } from "@/types";
 
 export default function ProfileView() {
+    // Retrieving cached user data
+    const queryClient = useQueryClient()
+    const data : User = queryClient.getQueryData(['user'])!
+
+    const { register, handleSubmit, formState: {errors} } = useForm<ProfileForm>({defaultValues: {
+        handle: data.handle,
+        description: data.description
+    }})
+
+    const handleUserProfileForm = (formData: ProfileForm) => {
+        console.log(formData)
+    }
 
     return (
         <form 
             className="bg-white p-10 rounded-lg space-y-5"
-            onSubmit={() => {}}
+            onSubmit={handleSubmit(handleUserProfileForm)}
         >
             <legend className="text-2xl text-slate-800 text-center">Edit profile</legend>
             <div className="grid grid-cols-1 gap-2">
@@ -17,7 +32,12 @@ export default function ProfileView() {
                     type="text"
                     className="border-none bg-slate-100 rounded-lg p-2"
                     placeholder="Handle or username"
+                    {...register('handle', {
+                        required: 'Username is required'
+                    })}
                 />
+
+                {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
             </div>
 
             <div className="grid grid-cols-1 gap-2">
@@ -27,7 +47,10 @@ export default function ProfileView() {
                 <textarea
                     className="border-none bg-slate-100 rounded-lg p-2"
                     placeholder="Your description"
+                    {...register('description')}
                 />
+
+                {errors.description && <ErrorMessage>{errors.description.message}</ErrorMessage>}
             </div>
 
             <div className="grid grid-cols-1 gap-2">
