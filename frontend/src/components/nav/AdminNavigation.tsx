d12af78161d2cd2ded64e53f42bb69d5a2d8559c
Brian Landler <brianlandler@gmail.com>
2025-05-24 16:27:34 +0200

    Changed invalidateQueries for removeQueries after logout to prevent cached data from being visible when logging in as a new user

--- frontend/src/components/nav/AdminNavigation.tsx
+++ frontend/src/components/nav/AdminNavigation.tsx
@@ -1,12 +1,15 @@
 import { useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import { PrimaryButton } from "../buttons";
 
 export default function AdminNavigation() {
+    const navigate = useNavigate()
     const queryClient = useQueryClient()
     
     const logout = () => {
         localStorage.removeItem('AUTH_TOKEN')
-        queryClient.invalidateQueries({queryKey: ['user']})
+        queryClient.removeQueries({queryKey: ['user']})
+        navigate('/auth/login')
     }
 
     return (
